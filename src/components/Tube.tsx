c3c4dc544c7774e9fe3b772d5957c9f4e9109390
chauchakching <chauchakching@gmail.com>
2023-11-19 13:51:32 +0800

    add selected tube animation

--- src/components/Tube.tsx
+++ src/components/Tube.tsx
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion';
-import { range } from '../utils/fp';
+import { motion } from "framer-motion";
+import { range } from "../utils/fp";
 
 export const Tube = ({
   size,
@@ -14,19 +14,34 @@ export const Tube = ({
 }) => {
   return (
     <motion.div
-      className={`flex flex-col border-t-white border-2 w-7 cursor-pointer ${
-        selected ? 'border-current' : ''
-      }`}
+      className={`flex flex-col border-b-2 border-l-2 border-r-2 rounded-b-full w-7 cursor-pointer`}
       onClick={onClick}
-      animate={{ scale: selected ? 1.1 : 1 }}
-      transition={{ type: 'spring', stiffness: 1500, damping: 30 }}
+      animate={
+        selected
+          ? { scale: 1.1, boxShadow: "6px 6px 8px rgba(0,0,0,0.2)" }
+          : { scale: 1, boxShadow: "0px 0px 0px rgba(0,0,0,0)" }
+      }
+      transition={{ type: "spring", stiffness: 1500, damping: 30 }}
     >
-      <div className={`w-3 h-3`}></div>
+      <div className={`w-6 h-3`}></div>
       {range(0, size - 1)
         .reverse()
         .map((i: number) => {
-          const color = colors[i] ? `bg-${colors[i]}` : '';
-          return <div key={i} className={`w-6 h-6 ${color}`}></div>;
+          const color = colors[i] ? `bg-${colors[i]}` : "";
+          return (
+            <div
+              key={i}
+              className={`w-6 h-6 ${color}`}
+              style={
+                i === 0
+                  ? {
+                      borderBottomLeftRadius: 12,
+                      borderBottomRightRadius: 12,
+                    }
+                  : {}
+              }
+            ></div>
+          );
         })}
     </motion.div>
   );
