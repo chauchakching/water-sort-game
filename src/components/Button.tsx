2be56e2e53052c1c9273740621d60edb083cf7e8
chauchakching <chauchakching@gmail.com>
2023-11-19 15:35:34 +0800

    add button hover style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,11 +5,10 @@ export const Button = (props: React.ComponentProps<typeof motion.button>) => {
   return (
     <motion.button
       type="button"
+      whileHover={{ backgroundColor: '#f9f9f9' }}
       whileTap={{ scale: 0.98, backgroundColor: '#f9f9f9' }}
       {...props}
-      className={`border px-2 rounded ml-4 bg-white hover:bg-gray-50 ${
-        props.className ?? ''
-      }`}
+      className={`border px-2 rounded ml-4 bg-white ${props.className ?? ''}`}
     ></motion.button>
   );
 };
